769d220bc310a3dcd1fb3dd56d32c75602f3058f
Hayk03 <forappleid1m@gmail.com>
2026-03-10 23:21:22 +0300

    added basic commands without execution

--- include/commands/BasicCommands.h
+++ include/commands/BasicCommands.h
@@ -1,4 +1,46 @@
 #ifndef FILEMANAGER_PROJECT_BASICCOMMANDS_H
 #define FILEMANAGER_PROJECT_BASICCOMMANDS_H
+#pragma once
+
+#include <string>
+#include <vector>
+#include "Command.h"
+
+namespace tfm {
+
+    class HelpCommand : public Command {
+    public:
+        bool execute(const std::vector<std::string>& args) override;
+        std::string getName() const override;
+        std::string getDescription() const override;
+        std::string getUsage() const override;
+    };
+
+    class PwdCommand : public Command {
+    public:
+        bool execute(const std::vector<std::string>& args) override;
+        std::string getName() const override;
+        std::string getDescription() const override;
+        std::string getUsage() const override;
+    };
+
+    class CdCommand : public Command {
+    public:
+        bool execute(const std::vector<std::string>& args) override;
+        std::string getName() const override;
+        std::string getDescription() const override;
+        std::string getUsage() const override;
+    };
+
+    class LsCommand : public Command {
+    public:
+        bool execute(const std::vector<std::string>& args) override;
+        std::string getName() const override;
+        std::string getDescription() const override;
+        std::string getUsage() const override;
+    };
+
+}
+
 
 #endif //FILEMANAGER_PROJECT_BASICCOMMANDS_H
--- include/commands/Command.h
+++ include/commands/Command.h
@@ -1,4 +1,22 @@
 #ifndef FILEMANAGER_PROJECT_COMMAND_H
 #define FILEMANAGER_PROJECT_COMMAND_H
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace tfm {
+
+    class Command {
+    public:
+        virtual ~Command() = default;
+
+        virtual bool execute(const std::vector<std::string>& args) = 0;
+        virtual std::string getName() const = 0;
+        virtual std::string getDescription() const = 0;
+        virtual std::string getUsage() const = 0;
+    };
+
+}
 
 #endif //FILEMANAGER_PROJECT_COMMAND_H
