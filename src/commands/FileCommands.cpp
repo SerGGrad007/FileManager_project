e26719f98315eb50bccbe86ecc0bf6b6f1ebb7ca
vlad1k112 <zavgorodvlad@bk.ru>
2026-03-12 17:29:18 +0300

    TouchCommand::execute

--- src/commands/FileCommands.cpp
+++ src/commands/FileCommands.cpp
@@ -28,4 +28,18 @@ namespace tfm {
     std::string MkdirCommand::getUsage() const {
         return "mkdir <directory_name>";
     }
+    bool TouchCommand::execute(const std::vector<std::string>& args) {
+        if (args.size()!= 1) {
+            std::cerr <<"Usage: " << getUsage() << '\n';
+            return false;
+        }
+        auto error = FileSystemService::createFile(args[0]);
+        if (error.has_value()) {
+            std:: cerr << "Error: " << error.value() << '\n';
+            return false;
+        }
+
+        std::cout << "File created successfully\n";
+        return true;
+    }
 }
